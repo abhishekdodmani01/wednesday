c178d4ba67c54624f2133d29334993d624104224
abhishekdodmani01 <abhiuppi012.com>
2023-02-15 13:05:37 +0530

    push

---
+++ Untitled1.java
@@ -0,0 +1,11 @@
+class Employee
+{
+static String emp name="manjuu";
+static int String salary="20000";
+	public static void main (Sting[] args)
+	{
+		System.out.println("the emp name is "+emp manju);
+		System.out.println("the employee sallary is "+salary);
+	}
+}
+
